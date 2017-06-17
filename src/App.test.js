1929ab99f44b13fdeee0333426a35589e8863e80
Nick Little <n14little@gmail.com>
2017-06-17 18:02:44 -0500

    user should be able to insert nickels, dimes, and quarters

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+test('renders without crashing', () => {
+  shallow(<App />);
+});
+
+test('should have a nickel, dime and quarter button so that the user can give the machine that amount of money', () => {
+  const app = shallow(<App />);
+  const nickel = <button type="button">Nickel</button>;
+  const dime = <button type="button">Dime</button>;
+  const quarter = <button type="button">Quarter</button>;
+
+  expect(app.contains(nickel)).toEqual(true);
+  expect(app.contains(dime)).toEqual(true);
+  expect(app.contains(quarter)).toEqual(true);
 });
