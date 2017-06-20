a8cc271e4b724316761c6110e4e1e154e48bc1c3
Nick Little <n14little@gmail.com>
2017-06-20 09:35:25 -0500

    react redux provider needs a store and an app

--- src/main/init.js
+++ src/main/init.js
@@ -1,10 +1,17 @@
+import all from './reducers/all';
+import App from './App';
+
 import React from 'react';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 const Init = (props) => {
+  const store = createStore(all.reduce);
   return (
-    <Provider />
-  )
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 };
 
 export default Init;
--- src/test/init.spec.js
+++ src/test/init.spec.js
@@ -1,10 +1,51 @@
 import Init from '../main/init';
+import App from '../main/App';
+import all from '../main/reducers/all';
 
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import * as redux from 'redux';
 
-test('should create react-redux provider', () => {
-  const provider = shallow(<Init />);
-  expect(provider.find(Provider)).toHaveLength(1);
-})
+import sinon from 'sinon';
+let sandbox = sinon.sandbox.create();
+
+describe('provider', () => {
+  let store;
+
+  beforeAll(() => {
+    const reducer = sandbox.stub(all, 'reduce');
+
+    // I'm torn here.
+    // I don't really care what createStore returns.
+    // I only care that it is used for my provider's props.
+    // However, I don't like seeing the console warnings either.
+    // They muddy the output.
+    store = {
+      subscribe: () => {},
+      dispatch: () => {},
+      getState: () => {}
+    };
+    sandbox.stub(redux, 'createStore').withArgs(reducer).returns(store);
+  });
+
+  test('should create react-redux provider', () => {
+    const init = shallow(<Init />);
+    expect(init.find(Provider)).toHaveLength(1);
+  });
+
+  test('should create provider with a store', () => {
+    const init = shallow(<Init />);
+    const provider = init.find(Provider);
+
+    expect(provider.props().store).toBe(store);
+  });
+
+  test('should create provider with our app', () => {
+    const init = shallow(<Init />);
+    const provider = init.find(Provider);
+
+    expect(provider.children()).toHaveLength(1);
+    expect(provider.children(App)).toHaveLength(1);
+  });
+});
