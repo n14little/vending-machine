01c3daa959c97aa831cf9f728d80b4009d5b68ed
Nick Little <n14little@gmail.com>
2017-06-22 06:43:07 -0500

    clean up products view

--- src/main/views/products.js
+++ src/main/views/products.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import numeral from 'numeral';
+
+const colaValue = 1.00;
+const chipsValue = 0.50;
+const candyValue = 0.65;
+const items = {
+  cola: { name: 'Cola', value: colaValue },
+  chips: { name: 'Chips', value: chipsValue },
+  candy: { name: 'Candy', value: candyValue }
+};
+
+const clickHandler = (dispatch, item) => {
+  dispatch({
+    type: 'PURCHASE',
+    item: item
+  });
+};
 
 const Products = (props) => {
   const  { dispatch } = props;
+  const colaText = `Cola - ${numeral(colaValue).format('$0.00')}`;
+  const chipsText = `Chips - ${numeral(chipsValue).format('$0.00')}`;
+  const candyText = `Candy - ${numeral(candyValue).format('$0.00')}`;
+  
   return (
     <div>
-      <button id="cola" type="button" onClick={() => dispatch({ type: 'PURCHASE', item: { name: 'Cola', value: 1.00}})}>Cola - $1.00</button>
-      <button id="chips" type="button" onClick={() => dispatch({ type: 'PURCHASE', item: { name: 'Chips', value: 0.50}})}>Chips - $0.50</button>
-      <button id="candy" type="button" onClick={() => dispatch({ type: 'PURCHASE', item: { name: 'Candy', value: 0.65}})}>Candy - $0.65</button>
+      <button id="cola" type="button" onClick={clickHandler.bind(this, dispatch, items.cola)}>{colaText}</button>
+      <button id="chips" type="button" onClick={clickHandler.bind(this, dispatch, items.chips)}>{chipsText}</button>
+      <button id="candy" type="button" onClick={clickHandler.bind(this, dispatch, items.candy)}>{candyText}</button>
     </div>
   );
-}
+};
 
 export default Products;
