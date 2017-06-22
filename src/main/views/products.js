1c391c37655174a1e73a3fbca868677fd6130caa
Nick Little <n14little@gmail.com>
2017-06-22 06:28:53 -0500

    product buttons should fire purchase actions

--- src/main/views/products.js
+++ src/main/views/products.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Products = () => {
+const Products = (props) => {
+  const  { dispatch } = props;
   return (
     <div>
-      <button id="cola" type="button">Cola - $1.00</button>
-      <button id="chips" type="button">Chips - $0.50</button>
-      <button id="candy" type="button">Candy - $0.65</button>
+      <button id="cola" type="button" onClick={() => dispatch({ type: 'PURCHASE', item: { name: 'Cola', value: 1.00}})}>Cola - $1.00</button>
+      <button id="chips" type="button" onClick={() => dispatch({ type: 'PURCHASE', item: { name: 'Chips', value: 0.50}})}>Chips - $0.50</button>
+      <button id="candy" type="button" onClick={() => dispatch({ type: 'PURCHASE', item: { name: 'Candy', value: 0.65}})}>Candy - $0.65</button>
     </div>
   );
 }
--- src/test/views/products.spec.js
+++ src/test/views/products.spec.js
@@ -3,12 +3,18 @@ import Products from '../../main/views/products';
 import React from 'react';
 import { shallow } from 'enzyme';
 import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 chai.should();
+chai.use(sinonChai);
 
-let products;
+let products,
+    dispatch,
+    sandbox = sinon.sandbox.create();
 
 beforeEach(() => {
-  products = shallow(<Products />);
+  dispatch = sandbox.stub();
+  products = shallow(<Products dispatch={dispatch}/>);
 });
 
 test('there are three products (cola, chips and candy)', () => {
@@ -20,6 +26,51 @@ test('cola button', () => {
 
   cola.should.have.lengthOf(1);
   cola.text().should.equal('Cola - $1.00');
+});
+
+test('cola button should dispatch PURCHASE with product and price', () => {
+  const cola = products.find('#cola');
+
+  cola.simulate('click');
+
+  dispatch.should.have.been.calledOnce;
+  dispatch.should.have.been.calledWith({
+    type: 'PURCHASE',
+    item: {
+      name: 'Cola',
+      value: 1.00
+    }
+  })
+});
+
+test('chips button should dispatch PURCHASE with product and price', () => {
+  const chips = products.find('#chips');
+
+  chips.simulate('click');
+
+  dispatch.should.have.been.calledOnce;
+  dispatch.should.have.been.calledWith({
+    type: 'PURCHASE',
+    item: {
+      name: 'Chips',
+      value: 0.50
+    }
+  })
+});
+
+test('candy button should dispatch PURCHASE with product and price', () => {
+  const candy = products.find('#candy');
+
+  candy.simulate('click');
+
+  dispatch.should.have.been.calledOnce;
+  dispatch.should.have.been.calledWith({
+    type: 'PURCHASE',
+    item: {
+      name: 'Candy',
+      value: 0.65
+    }
+  })
 });
 
 test('chips button', () => {
