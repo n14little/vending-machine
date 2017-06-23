8b6de093838c69cb31599d59ba42df34de327548
Nick Little <n14little@gmail.com>
2017-06-23 06:37:38 -0500

    successful purchase should fire successful purchase action

--- src/main/views/payment-processor.js
+++ src/main/views/payment-processor.js
@@ -1,11 +1,19 @@
+import actions from '../constants/actions';
+
 import React from 'react';
 import numeral from 'numeral';
 
-const PaymentProcessor = ({ purchase, currentBalance }) => {
+function clickHandler(dispatch) {
+  dispatch({
+    type: actions.SUCCESSFUL_PURCHASE,
+  });
+}
+
+const PaymentProcessor = ({ purchase, currentBalance, dispatch }) => {
   return (
     <div>
       <p>{purchase.value === currentBalance ? 'THANK YOU' : `PRICE: ${numeral(purchase.value).format('$0.00')}`}</p>
-      <button type="button">OK</button>
+      <button type="button" onClick={clickHandler.bind(this, dispatch)}>OK</button>
     </div>
   );
 };
--- src/test/views/payment-processor.spec.js
+++ src/test/views/payment-processor.spec.js
@@ -1,9 +1,15 @@
 import PaymentProcessor from '../../main/views/payment-processor';
+import actions from '../../main/constants/actions';
 
 import React from 'react';
 import { shallow } from 'enzyme';
 import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
 chai.should();
+chai.use(sinonChai);
+const sandbox = sinon.sandbox.create();
 
 const colaValue = 1.00;
 const chipsValue = 0.50;
@@ -39,10 +45,18 @@ test('should display price of the item if not enough money in the machine', () =
 test('should include a button that dismisses dialog and removes the purchase from state when enough money', () => {
   const purchase = items.chips;
   const currentBalance = chipsValue;
+  const dispatch = sandbox.stub();
 
-  const paymentProcessor = shallow(<PaymentProcessor purchase={purchase} currentBalance={currentBalance} />);
+  const paymentProcessor = shallow(<PaymentProcessor purchase={purchase} currentBalance={currentBalance} dispatch={dispatch}/>);
   const dismissButton = paymentProcessor.find('button[type="button"]');
 
   dismissButton.should.have.lengthOf(1);
   dismissButton.text().should.equal('OK');
+
+  dismissButton.simulate('click');
+
+  dispatch.should.have.been.calledOnce;
+  dispatch.should.have.been.calledWith({
+    type: actions.SUCCESSFUL_PURCHASE,
+  });
 });
