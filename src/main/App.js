99f261f26c9f1b1df904900e0a795af146cc793a
Nick Little <n14little@gmail.com>
2017-06-22 22:19:10 -0500

    app should have a payment processor when purchases are made

--- src/main/App.js
+++ src/main/App.js
@@ -1,17 +1,25 @@
 import CoinMachine from './views/coin-machine';
 import Products from './views/products';
+import PaymentProcessor from './views/payment-processor';
 import '../css/App.css';
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+function getPaymentProcessor(purchase, currentBalance) {
+  if (purchase) {
+    return (<PaymentProcessor purchase={purchase} currentBalance={currentBalance} />);
+  }
+}
+
 export class App extends Component {
   render() {
-    const { currentBalance, dispatch } = this.props;
+    const { currentBalance, dispatch, purchase } = this.props;
     return (
       <div className="App">
         <Products dispatch={dispatch}/>
         <CoinMachine currentBalance={currentBalance} dispatch={dispatch}/>
+        {getPaymentProcessor(purchase, currentBalance)}
       </div>
     );
   }
--- src/test/App.spec.js
+++ src/test/App.spec.js
@@ -1,6 +1,7 @@
 import { App } from '../main/App';
 import Products from '../main/views/products';
 import CoinMachine from '../main/views/coin-machine';
+import PaymentProcessor from '../main/views/payment-processor';
 import React from 'react';
 import { shallow } from 'enzyme';
 import chai from 'chai';
@@ -19,4 +20,21 @@ test('app should have a Products component', () => {
   const currentBalance = 0;
   const app = shallow(<App dispatch={dispatch} currentBalance={currentBalance}/>);
   app.contains(<Products dispatch={dispatch}/>).should.equal(true);
+});
+
+test('app should have a PaymentProcessor component if a purchase has been made', () => {
+  const purchase = {
+    name: 'blah',
+    value: 0.5
+  };
+  const currentBalance = 0.5;
+
+  const app = shallow(<App purchase={purchase} currentBalance={currentBalance}/>);
+  app.contains(<PaymentProcessor purchase={purchase} currentBalance={currentBalance}/>).should.equal(true);
+});
+
+test('should not have a PaymentProcessor component if a purchase has not been made', () => {
+  const app = shallow(<App />);
+  app.children().should.have.lengthOf(2);
+  app.contains(<PaymentProcessor purchase={undefined} currentBalance={undefined}/>).should.equal(false);
 });
